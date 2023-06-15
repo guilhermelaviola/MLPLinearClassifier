214633ab891c6b7872e927cc833bc664d7fa6ec6
guilherme <guilhermelaviola96@gmail.com>
2023-06-15 12:48:51 -0300

    Updated the second module and also updated the README.md file.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MLPLinearClassifier
 Multi Layer Perceptron for Classification with Java.
+
+##1. Building a Linear Classifier (MLP) With Deeplearning4j
+
+###Multi Layer Perceptron for Classification
+- Exploration of Data
+- Demo of Java Class to Classify data
+
+##2. Preprocessing Data for Machine Learning With Datavec & Spark
+
+###Datavec Transform
+- Read file
+- Select Fields
+- Transform to Numeric
+
--- src/com/java/ml/storm_reports_record_reader/StormReportsRecordReader.java
+++ src/com/java/ml/storm_reports_record_reader/StormReportsRecordReader.java
@@ -1,10 +1,21 @@
 package com.java.ml.storm_reports_record_reader;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.xml.validation.Schema;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.datavec.api.records.reader.BaseRecordReader;
+import org.datavec.api.records.reader.impl.inmemory.InMemoryRecordReader;
 import org.datavec.api.transform.TransformProcess;
+import org.datavec.api.writable.Writable;
+import org.datavec.spark.transform.SparkTransformExecutor;
+import org.datavec.spark.transform.misc.StringToWritablesFunction;
+import org.datavec.spark.transform.misc.WritablesToStringFunction;
+
 
 public class StormReportsRecordReader {
 
@@ -51,5 +62,24 @@ public class StormReportsRecordReader {
 			System.out.println("--- Schema after step " + i + " (" + tp.getActionList().get(i) + ")--");
 			System.out.println(tp.getSchemaAfterStep(i));
 		}
+		
+		SparkConf sparkConf = new SparkConf();
+		sparkConf.setMaster("local[*]");
+		sparkConf.setAppName("Storm Reports Record Reader Transform");
+		JavaSparkContext sc = new JavaSparkContext(sparkConf);
+		
+		/*
+		 * Getting our data into a Spark RDD and transforming
+		 * that Spark RDD using our transform process
+		 */
+		
+		// Reading the data file
+		JavaRDD<String> lines = sc.textFile(inputPath);
+		// Converting it to Writable
+		JavaRDD<List<Writable>> stormReports =  lines.map(new StringToWritablesFunction(new InMemoryRecordReader(null)));
+		// Running our transform process
+		JavaRDD<List<Writable>> processed = SparkTransformExecutor.execute(stormReports, tp);
+		// Converting the Writable back to String for export
+		JavaRDD<String> toSave = processed.map(new WritablesToStringFunction(","));
 	}
 }
